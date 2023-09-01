07e9ad0c3e50d5770b7ea272a8608bcad1af3589
KJS <kjs94120@pusan.ac.kr>
2023-09-01 10:07:15 +0900

    Apply .gitignore

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 def main():
 
     st.markdown("# Hi")
     st.write(np.__version__)
+    st.write(pd.__version__)
 
 if __name__=="__main__":
     main()
